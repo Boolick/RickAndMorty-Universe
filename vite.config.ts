2ac529ec53d98ba15ecd9a8a8069e5d6500093ae
Boolick <boolick38@gmail.com>
2024-09-14 00:42:55 +0300

    fix: base, name of repository in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/Boolick/',
+  base: '/RickAndMorty-Universe/',
   plugins: [react()],
   css: {
     modules: {
