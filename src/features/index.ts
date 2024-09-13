7a5452f1f237f301e37034375b19e6c5885b0eb1
Boolick <boolick38@gmail.com>
2024-09-14 01:13:43 +0300

    docs: Deploy to gh-pages

--- src/features/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as likesReducer } from './likes/model/likesSlice';
-export { default as LikeButton } from './likes/ui/like-button/LikeButton';
-export { default as LikeFilterButton } from './likes/ui/like-filter-button/LikeFilterButton';
-export { default as deleteReducer } from './cards/model/deleteSlice';
-export { default as DeleteButton } from './cards/ui/DeleteButton';
-export { default as RestoreButton } from './cards/ui/RestoreButton';
-export { restoreAllCards } from './cards/model/deleteSlice';
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFoundPage = (): JSX.Element => {
-  return <div>404 NotFound</div>;
-};
-
-export default NotFoundPage;
