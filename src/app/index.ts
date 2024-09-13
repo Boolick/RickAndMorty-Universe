2577868eb76565300bc178d03fd9690f2cb8c950
Boolick <boolick38@gmail.com>
2024-09-14 01:57:55 +0300

    fix: build deploy

--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as store } from './store/store';
-export type { RootState, AppDispatch } from './store/store';
--- src/shared/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Header } from '../ui/header/Header';
-// export button
-// export Loader
